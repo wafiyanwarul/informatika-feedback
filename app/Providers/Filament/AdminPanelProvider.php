9180fa680ed596c5d2f5314af07388379a40ca44
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2025-05-20 12:27:50 +0700

    :tada:feat: update deployement

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,6 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->profile()
             ->spa()
             ->unsavedChangesAlerts()
             ->colors([
