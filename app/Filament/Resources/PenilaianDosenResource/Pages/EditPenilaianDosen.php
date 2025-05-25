689e27903d6b3e479989b1bb98b3eaa1f79ec923
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2025-05-25 20:28:35 +0700

    feat: update response and penilaian dosen panels

---
+++ app/Filament/Resources/PenilaianDosenResource/Pages/EditPenilaianDosen.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Filament\Resources\PenilaianDosenResource\Pages;
+
+use App\Filament\Resources\PenilaianDosenResource;
+use Filament\Actions;
+use Filament\Resources\Pages\EditRecord;
+
+class EditPenilaianDosen extends EditRecord
+{
+    protected static string $resource = PenilaianDosenResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\DeleteAction::make(),
+        ];
+    }
+}
