51202e9feac2901878bd1140826ac94ffe157f54
Wafiy Anwarul HIkam <95902360+wafiyanwarul@users.noreply.github.com>
2025-05-26 10:33:19 +0700

    feat: update theme and remove filament information at main dashboard

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -33,10 +33,20 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
             ->profile(EditProfile::class)
             ->spa()
+            ->sidebarCollapsibleOnDesktop()
+            ->navigationGroups([
+                'Survey Management',
+                'User Management',
+                'System Settings',
+                'Reports & Analytics',
+            ])
             ->unsavedChangesAlerts()
             ->colors([
-                'primary' => Color::Amber,
+                'primary' => Color::Blue,
+                'gray' => Color::Slate,
             ])
+            ->defaultThemeMode(\Filament\Enums\ThemeMode::Dark)
+            ->brandName('Informatics Feedback System')
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
@@ -45,7 +55,7 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\FilamentInfoWidget::class,
+                // Widgets\FilamentInfoWidget::class,
                 MainStatsOverview::class,
             ])
             ->middleware([
